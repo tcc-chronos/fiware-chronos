490bce1a16ad2bf83c340728e808b4aa5b6ead8e
Vinícius Benevides <viniciusbenevidesdasilva16@gmail.com>
2025-09-14 20:43:08 -0300

    refactor: remove old hexagon architecture

--- pkg/utils/versions.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-
-def pkg_version(name: str) -> str:
-    """Retorna a versão instalada de um pacote ou 'unknown'."""
-    try:
-        return version(name)
-    except PackageNotFoundError:
-        return "unknown"
